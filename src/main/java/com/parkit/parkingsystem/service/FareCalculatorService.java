8e8ec8e5d6115d03631e425fe0b585f6c57e4c08
Akonedev <akonedev@gmail.com>
2023-03-19 04:05:22 +0100

    Add Corrections for tests to Pass 2

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -14,10 +14,10 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        Instant inHour = ticket.getInTime().toInstant();
-        Instant outHour = ticket.getOutTime().toInstant();
+        final Instant inHour = ticket.getInTime().toInstant();
+        final Instant outHour = ticket.getOutTime().toInstant();
 
-        long duration = Duration.between(inHour, outHour).toMinutes();
+        final long duration = Duration.between(inHour, outHour).toMinutes();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
         if (duration<30) {
@@ -25,13 +25,13 @@ public class FareCalculatorService {
         } else {
             switch (ticket.getParkingSpot().getParkingType()) {
                 case CAR: {
-                   // ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                    ticket.setPrice((duration * (Fare.CAR_RATE_PER_HOUR / 60))
-                            - (Fare.CAR_RATE_PER_HOUR / 2));
+                    final double standardPrice = (duration * (Fare.CAR_RATE_PER_HOUR / 60)) - (Fare.CAR_RATE_PER_HOUR / 2);
+                    ticket.setPrice((duration * (Fare.CAR_RATE_PER_HOUR / 60)) - (Fare.CAR_RATE_PER_HOUR / 2));
                     break;
                 }
                 case BIKE: {
-                    //ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                    final double standardPrice = (duration * (Fare.BIKE_RATE_PER_HOUR / 60))
+                            - (Fare.BIKE_RATE_PER_HOUR / 2);
                     ticket.setPrice((duration * (Fare.BIKE_RATE_PER_HOUR / 60))
                             - (Fare.BIKE_RATE_PER_HOUR / 2));
                     break;
